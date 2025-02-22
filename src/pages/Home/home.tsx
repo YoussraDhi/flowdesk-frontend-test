5b7ba5de991509bf080cfb0b8a322579e4732d97
YoussraDhi <youssra@groover.co>
2025-02-22 14:50:19 +0100

    styling

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -7,18 +7,28 @@ import TickerPairForm from "./tickerPairForm";
 
 function Home(): React.ReactElement {
   return (
-    <div className="home">
-      <Aurora colorStops={["#fca311", "#14213d", "#fca311"]} amplitude={0.6} />
-      <GlitchText
-        speed={1}
-        enableShadows={true}
-        enableOnHover={true}
-        fontSize="52px"
-      >
-        Crypto Tracker
-      </GlitchText>
+    <main>
+      <div className="top-section">
+        <Aurora
+          colorStops={["#fca311", "#14213d", "#fca311"]}
+          amplitude={0.6}
+        />
+        <GlitchText
+          speed={1}
+          enableShadows={true}
+          enableOnHover={true}
+          fontSize="52px"
+        >
+          Crypto Tracker
+        </GlitchText>
+        <button>
+          <a href="/dashboard">View Market</a>
+        </button>
+        OR
+        <h2>Select a Pair</h2>
+      </div>
       <TickerPairForm />
-    </div>
+    </main>
   );
 }
 
